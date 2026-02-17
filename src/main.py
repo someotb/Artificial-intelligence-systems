261860f10b4b0c874c0e2c1a7883a70f62946faa
someotb <llyryk934@gmail.com>
2026-02-17 10:33:56 +0700

    feat: add more info about dataset

--- src/main.py
+++ src/main.py
@@ -4,5 +4,18 @@ DATA_FILE = "../data/drugLibTest_raw.tsv"
 
 data = pd.read_csv(DATA_FILE, sep="\t", engine="python")
 
-print(data.head())
-print(data.info())
+print("Общая информация о dataset:")
+data.info()
+print("\n")
+
+
+print("Первые 5 строк:\n", data.head(), "\n")
+print("Последние 5 строк:\n", data.tail(), "\n")
+
+print("\nСтатистика по числовым колонкам:")
+print(data.describe(), "\n")
+
+print("Кол-во строк, столбцов: ", data.shape, "\n")
+print("Имена столбцов:\n", data.columns, "\n")
+
+# print(data.iloc[0])
