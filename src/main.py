c25221b5d9ec6ae957d22a5f57c69c7d711f9860
someotb <llyryk934@gmail.com>
2026-05-05 12:46:12 +0700

    feat(lab8): add additional task

--- src/main.py
+++ src/main.py
@@ -18,8 +18,35 @@ y_test  = pd.read_csv("data/lab6_Y_test.csv").values.ravel()
 X_train_1d = X_train[["effectiveness"]].values
 X_test_1d  = X_test[["effectiveness"]].values
 
+min_mae = 100
+min_med = 100
+best_degree = []
+degrees = []
+maes = []
+
+for deg in range(1, 21):
+    # Полиномиальные признаки
+    poly_test = PolynomialFeatures(degree=deg)
+    X_train_poly_t = poly_test.fit_transform(X_train_1d)
+    X_test_poly_t  = poly_test.transform(X_test_1d)
+
+    # Обучение
+    model_t = LinearRegression()
+    model_t.fit(X_train_poly_t, y_train)
+
+    # Предсказание и метрики
+    y_pred = model_t.predict(X_test_poly_t)
+    mae     = mean_absolute_error(y_test, y_pred)
+
+    degrees.append(deg)
+    maes.append(mae)
+
+    if min_mae > mae:
+        min_mae = mae
+        best_degree.append(deg)
+
 # Полиномиальные признаки
-poly = PolynomialFeatures(degree=4)
+poly = PolynomialFeatures(degree=best_degree[-1])
 X_train_poly = poly.fit_transform(X_train_1d)
 X_test_poly  = poly.transform(X_test_1d)
 
@@ -28,9 +55,10 @@ model = LinearRegression()
 model.fit(X_train_poly, y_train)
 
 # Предсказание и метрики
-from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error, median_absolute_error, explained_variance_score
-
 y_pred = model.predict(X_test_poly)
+
+print(f"Лучшее значение MAE: {min_mae} для degree = {best_degree[-1]}")
+print(f"Все значения degree: {[deg for deg in best_degree]}\n")
 
 mae     = mean_absolute_error(y_test, y_pred)
 mse     = mean_squared_error(y_test, y_pred)
@@ -56,10 +84,18 @@ y_grid = model.predict(poly.transform(X_grid))
 # График
 plt.figure(figsize=(9, 5))
 plt.scatter(X_test_1d, y_test, color="red", label="Реальные значения", zorder=5)
-plt.plot(X_grid, y_grid, color="blue", linewidth=2, label="Poly degree=4")
+plt.plot(X_grid, y_grid, color="blue", linewidth=2, label=f"Poly degree={best_degree[-1]}")
 plt.xlabel("Effectiveness")
 plt.ylabel("Rating")
 plt.title("Polynomial Regression — Effectiveness vs Rating")
 plt.legend()
 plt.tight_layout()
+
+plt.figure()
+plt.plot(degrees, maes, label="Зависимость MAE от degrees")
+plt.xlabel("Degrees values")
+plt.ylabel("MAE")
+plt.title("Зависимость MAE от degrees")
+plt.legend()
+
 plt.show()
